55709270beecb6aa002e7b5e7cb29a2e550e48be
ilhanTabak <herr.ilhantabak@gmail.com>
2023-03-01 12:46:50 +0100

    yeni dosya push edildi

--- src/Deneme/ortakDosya.java
+++ src/Deneme/ortakDosya.java
@@ -3,5 +3,6 @@ package Deneme;
 public class ortakDosya {
     public static void main(String[] args) {
         System.out.println("ortak dosya");
+        System.out.println("lokal degidiklik");
     }
 }
