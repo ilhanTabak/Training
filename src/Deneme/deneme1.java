74876246ceaa58bc66301a50c6b848e9c3c0544f
ilhanTabak <herr.ilhantabak@gmail.com>
2023-03-01 16:52:24 +0100

    b1 degisiklik

--- src/Deneme/deneme1.java
+++ src/Deneme/deneme1.java
@@ -4,6 +4,7 @@ public class deneme1 {
     public static void main(String[] args) {
         System.out.println("hallo");
         System.out.println("merhaba");
+        System.out.println("b1");
 
     }
 }
